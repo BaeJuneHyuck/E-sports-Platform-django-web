21e50ba41ba2d816c7884dec6744d5e9228b1415
kyj960901 <kyj960901@naver.com>
2020-05-25 17:54:27 +0900

    오버워치 API DB
    오버워치 API 데이터베이스 연동

---
+++ OW_parse/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OwParseConfig(AppConfig):
+    name = 'OW_parse'
--- esportsPlatform/settings.py
+++ esportsPlatform/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'disqus',
     'crispy_forms',
+    'OW_parse',
 ]
 
 MIDDLEWARE = [
