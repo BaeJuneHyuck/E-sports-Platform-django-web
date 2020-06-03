e21937797ba03a9946ed42f22608e41444f2ec09
배준혁 (JuneHyuck Bae) <37135285+BaeJuneHyuck@users.noreply.github.com>
2020-06-03 13:33:40 +0900

    대회 생성 페이지 관련 파일 추가

---
+++ competitions/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from .models import Competition
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+NOW = timezone.now()
+
+GAME=[
+    ('Overwatch', 'Overwatch'),
+    ('LOL', 'LOL'),
+]
+
+TOURNAMENT_TYPE = [
+    (-1, 'Single Elimination'),    # 1패 하면 탈락하는 토너먼트
+    ( -2, 'Double Elimination'),   # 패배시 패자전에서 한번더 기회있는 토너먼트
+    (2, 'Round Robin'),            # 모든 상대팀과 N번 경기를 치룬다
+]
+
+class CompetitionCreateForm(forms.ModelForm):
+    competition_name = forms.CharField(label='대회명', max_length=100 )
+    competition_text = forms.CharField(label='대회설명',max_length=600, widget=forms.Textarea)
+    competition_game = forms.CharField(label='대회종목', widget=forms.Select(choices=GAME))
+
+    tournament_type = forms.CharField(label='대회 방식', widget=forms.Select(choices=TOURNAMENT_TYPE))
+    required_tier = forms.IntegerField(label='최소 티어')
+    total_teams = forms.IntegerField(label='참여 팀 수')
+    date_start = forms.DateTimeField(label='대회 시작',widget = forms.TextInput(
+        attrs={'type': 'date'}
+    ))
+    date_end = forms.DateTimeField(label='대회 종료',widget=forms.TextInput(
+        attrs={'type': 'date'}
+    ))
+
+    attend_start = forms.DateTimeField(label='참가 신청 시작',widget=forms.TextInput(
+        attrs={'type': 'date'}
+    ))
+    attend_end = forms.DateTimeField(label='참가 신청 종료',widget=forms.TextInput(
+        attrs={'type': 'date'}
+    ))
+
+    is_public = forms.BooleanField(label='대회 공개', required=False)
+
+    class Meta:
+        model = Competition
+        fields = ['competition_name', 'competition_text', 'competition_game', 'required_tier','tournament_type', 'date_start','date_end','attend_start','attend_end','is_public']
+
+    def clean_date_start(self):
+        NOW = timezone.now()
+        date_start = self.cleaned_data['date_start']
+        if date_start <= NOW:
+            raise ValidationError('지난 시간은 선택할 수 없습니다.')
+        return date_start
